a1ab0efdd3330a27ffd84ad8b4da60ce64671593
Megan McGee <mtorau@gmail.com>
2021-09-04 19:29:27 -0500

    add step for lat and long outliers

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -35,6 +35,10 @@ def go(args):
     logger.info("Convert last_review to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
     
+    logger.info("Dropping outliers on latitude and longitude")
+    idx_lat_long = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx_lat_long].copy()
+    
     logger.info("Creating output artifact")
     df.to_csv('clean_sample.csv', index=False)
     artifact = wandb.Artifact(
